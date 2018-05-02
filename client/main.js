349ab418524cfbccdbf16be51c70245ccdb8fc52
Jenny Kinert <id14jkt@cs.umu.se>
2018-05-02 16:41:47 +0200

    som install things

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import React from 'react'
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
