fe79ea753a1e046887d22647c23ca52b75eca4fd
LeoBuren <leo.buren@hotmail.com>
2018-05-13 12:45:40 +0200

    Transferring files between computers.

--- imports/ui/components/App.js
+++ imports/ui/components/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import EmojiContainer from '../containers/emojiContainer'
 
 const App = () => (
   <div className="todocontainer container">
     <AddTodo />
     <VisibleTodoList />
     <Footer />
+    <EmojiContainer />
   </div>
 )
 
